bc7d0bda9ce4d8cfce6bd9937d42004f9f602251
Trent Baldwin <trentbaldwin@Trents-MacBook-Pro-2.local>
2018-12-01 09:58:15 -0600

    Update

--- LeetHacksSimulator/Player.cs
+++ LeetHacksSimulator/Player.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+//Yes
 namespace LeetHacksSimulator
 {
     public class Player
     {
         public Player()
         {
+
+        }
+        public String name;
+        public void addName(String name) 
+        {
+            Player.name = name;
         }
     }
 }
